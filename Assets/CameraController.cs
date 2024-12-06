479755d73b3eded9b6bb6135eef865e847d80458
SHIBINU001 <000928128@mohawkcollege.ca>
2024-12-06 17:45:32 -0500

    Finished Camera Controller
    Camera follow the player

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform player;
+    private Rigidbody playerRB;
+    public Vector3 Offset;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+     Vector3 playerForward = (playerRB.velocity + player.transform.forward).normalized;
+     transform.position = Vector3.Lerp(transform.position,
+     player.position + player.transform.TransformVector(Offset)
+     + playerForward * (-5f),
+     speed * Time.deltaTime);
+     transform.LookAt(player);
     }
 }
